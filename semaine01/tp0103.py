5515004a87f4077d5d9d351d7f2d2bde2c81e8c7
Norbert <norbetltagne@gmail.com>
2025-04-03 11:22:48 +0100

    progression_gestion des fichiers

--- semaine01/tp0103.py
+++ semaine01/tp0103.py
@@ -1,2 +1,9 @@
-import csv
+import pandas as pd
 
+#Trier les produits par prix croissant et sauvegarder le fichier trié
+
+#Charger les produits 'produits.csv'
+produits = pd.read_csv('produits.csv')
+
+
+produits.sort_values(by='prix', ascending=False, inplace=True)
