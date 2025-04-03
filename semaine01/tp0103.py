6a6952f9b54a8e997de4049650fdad766163e499
Norbert <norbetltagne@gmail.com>
2025-04-03 11:24:55 +0100

    Trie des fichiers en utilisant pandas

--- semaine01/tp0103.py
+++ semaine01/tp0103.py
@@ -5,5 +5,8 @@ import pandas as pd
 #Charger les produits 'produits.csv'
 produits = pd.read_csv('produits.csv')
 
-
 produits.sort_values(by='prix', ascending=False, inplace=True)
+
+produits.to_csv('produits_trie.csv', index=False) # Sauvegarder le fichier trié
+print(produits) # Afficher le fichier trié
+print("Le fichier trié a été sauvegardé sous le nom 'produits_trie.csv'")
